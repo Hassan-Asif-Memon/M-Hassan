6aa2439bc20c8a39d12ca4e8ab91424a598922e2
Hassan-Asif-Memon <hm0078275@gmail.com>
2026-06-02 17:05:32 +0500

    New Material

--- React/src/components/Api.jsx
+++ React/src/components/Api.jsx
@@ -3,18 +3,29 @@ import { useState } from "react";
 function Api() {
 const [query, setQuery] = useState("");
 const [images, setImages] = useState([]);
+const [loading, setLoading]=useState(false);
+const[searched, setSearched]=useState(false);
 const searchImages = async () => {
+  try {
+    setLoading(true);
+  
 const response = await axios.get("https://api.unsplash.com/search/photos", {
     // const response = await axios.get("", {
       params: { query: query, per_page: 20 },
-      headers: { Authorization: `Client-ID ${import.meta.env.VITE_Yasir}` },
+      headers: { Authorization: `Client-ID ${import.meta.env.VITE_Hassan}`, },
     });
     setImages(response.data.results);
-  };
+    setSearched(true);
+  } catch(error){
+    console.log(error);
+  }finally{
+    setLoading(false);
+  }
+};
   return (
-    <div className="container">
+    <div>
       <h1>Image Search App </h1>
-      <div className="search-box">
+      <div>
         <input
           type="text"
           placeholder="Search images..."
@@ -22,9 +33,13 @@ const response = await axios.get("https://api.unsplash.com/search/photos", {
           onChange={(e) => setQuery(e.target.value)}
         />
         <button onClick={searchImages}>Search</button>
+        {loading && <h3>Loading.....</h3>}
+        {!loading && searched && images.length === 0 && (
+  <h2>No images found.</h2>
+)}
       </div>
 
-      <div className="image-grid">
+      <div>
         {images.map((image) => (
           <div className="card" key={image.id}>
             <img src={image.urls.small} alt={image.alt_description} />
